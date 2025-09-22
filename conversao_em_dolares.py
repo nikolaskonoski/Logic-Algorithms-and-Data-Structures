cb1b7f172e29d7c9d4f216abb4f78d38786bc11e
Nikolas Konoski <nikolasmkonoski@gmail.com>
2025-09-21 22:11:00 -0300

    Quantidade necessária de tinta!

--- conversao_em_dolares.py
+++ conversao_em_dolares.py
@@ -1,3 +1,7 @@
+'''
+9) Faça um algoritmo que leia quanto dinheiro uma pessoa tem na carteira (em R$)
+e mostre quantos dólares ela pode comprar. Considere US$1,00 = R$3,45.
+'''
 valor_em_reais = float(input("Digite um valor em R$: "))
 
 cotacao_dolar = float(input("Digite a cotação do dólar: "))
