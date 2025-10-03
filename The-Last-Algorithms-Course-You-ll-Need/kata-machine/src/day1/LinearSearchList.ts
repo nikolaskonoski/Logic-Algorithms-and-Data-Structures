f6fc58a0a925dab7ae2d1e80f624daded954084b
Nikolas Konoski <nikolasmkonoski@gmail.com>
2025-10-03 19:08:15 -0300

    Gerando os exercicios para a aula 1

--- The-Last-Algorithms-Course-You-ll-Need/kata-machine/src/day1/LinearSearchList.ts
+++ The-Last-Algorithms-Course-You-ll-Need/kata-machine/src/day1/LinearSearchList.ts
@@ -1,3 +1,11 @@
 export default function linear_search(haystack: number[], needle: number): boolean {
-
+    // Percorre cada elemento do array 'haystack'.
+    for (let i = 0; i < haystack.length; ++i) {
+        // Se o elemento atual for igual ao 'needle', encontramos!
+        if (haystack[i] === needle) {
+            return true; // Retorna true e encerra a função.
+        }
+    }
+    // Se o loop terminar, significa que o 'needle' não foi encontrado.
+    return false;
 }
