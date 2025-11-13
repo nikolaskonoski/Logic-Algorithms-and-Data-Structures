621394f1b1d0894c759b2459fd3783958a321f66
Nikolas Konoski <nikolasmkonoski@gmail.com>
2025-11-13 15:13:27 -0300

    Aprendendo declaração de constantes

--- Curso_de_C++_CFB/estruturaBasicaDeUmPrograma.cpp
+++ Curso_de_C++_CFB/estruturaBasicaDeUmPrograma.cpp
@@ -1,5 +1,8 @@
 #include <iostream> //Biblioteca para entrada e saida de dados 
 
+#define pi 3.1415 //Declarando uma constante
+#define canal cout << "Canal Fessor Bruno\n\n"; //Aqui estamos definindo um comando para uma constante
+
 using namespace std; //Uma organização de pastas/bibliotecas separando-as por funcionalidades
 
 //Esta é a função principal que pode ser int pra retornar um valor 0,1 ou void pra ter um retorno nulo/vazio 
