f740e9e61b20d4bee807453941d9987e57cdd6da
Nikolas Konoski <nikolasmkonoski@gmail.com>
2025-11-16 11:35:48 -0300

    Inversão de valores em variáveis

---
+++ Curso_de_C++_CFB/invertendoValoresDeVariaveis.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num=10;
+
+    cout << num << "\n\n";
+    
+    //num = num * -1;
+    num=-num; //Muda o valor 
+
+    cout << -num << "\n\n"; //Usando aqui só estou alterando a saida 
+
+    return 0;
+}
+
+//São maneiras de inverter o valor das variáveis, podem ser feitos de formas diferentes, algumas formas invertem e atribuem o novo valor para aa variável
+// outras só mudam a saida.
+//Um exemplo de uso é fazer a troca de direção de eixo x para y em um jogo 
