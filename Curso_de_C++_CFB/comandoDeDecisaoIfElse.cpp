26f88748c0beb75a3a1607028b36aa6da6ab9c97
Nikolas Konoski <nikolasmkonoski@gmail.com>
2025-11-16 15:47:33 -0300

    Condicional If/Else

---
+++ Curso_de_C++_CFB/comandoDeDecisaoIfElse.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num1 = 10;
+    int num2 = 50;
+    char opc = 's';
+
+    if (*condição lógica)
+    {
+        cout << "Vou ao clube";
+    }
+    else
+    {
+        cout << "Vou ao cinema";
+    }
+
+    return 0;
+}
