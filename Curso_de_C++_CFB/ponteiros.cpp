661a308692ce76cc6d32b776899e1227c03d6e4e
Nikolas Konoski <nikolasmkonoski@gmail.com>
2025-12-02 10:45:45 -0300

    Ponteiros Parte: 2

--- Curso_de_C++_CFB/ponteiros.cpp
+++ Curso_de_C++_CFB/ponteiros.cpp
@@ -18,6 +18,32 @@ int main()
          << veiculo << "\n"
          << *pv;
 
+
+//Cada endereço de memória pula de 4 em 4 pois int = 4bytes, como a contagem é hexadecimal, o ultimo item se for após o 9, recebera uma letra (a, b,c...).
+    int *p;
+    int array[10];
+
+
+    *(p+=1); //Incrementando uma posição  
+    *p=10; //Atribuindo o valor 10 para a variável || o mesmo que fazer array[0] = 10;
+    cout << "\n" << p << "\n";
+
+    *(p+=1);
+    *p=20;
+    cout << "\n" << p << "\n";
+
+    p = &array[2]; //Manipulando de uma maneira sem tanta escala
+    cout << "\n" << p << "\n";
+
+    p = &array[3]; // *(p+=1)  faz um incremento da próxima posição de acordo com o tamanho (tipo) de bytes do ponteiro 
+    cout << "\n" << p << "\n";
+
+        
+
+
+
+
+
     return 0;
 }
 
@@ -29,9 +55,8 @@ Me possibilita acessar/alterar o valor da variável mesmo estando fora de um esc
 
 
 
+
 int * pn;
 pn = &num;
-
-
 
 */
