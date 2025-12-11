9b2c83d6a2c43e048349cd5780872818dd50ade8
Nikolas Konoski <nikolasmkonoski@gmail.com>
2025-12-11 16:41:47 -0300

    System

---
+++ Curso_de_C++_CFB/system.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stdlib.h> 
+
+using namespace std; 
+
+int main() {
+    system("pause"); 
+    system("color"); 
+    system("cls"); 
+    system("dir")
+}
+/* 
+São comandos que podem ser usados no prompt de comando também 
+Comandos que fazem algum tipo de interação com o programa 
+
+
+*/
